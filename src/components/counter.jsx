3fe4311903c4598ee32eed6444e17750a5df57f5
Nancy Thomas <linust95@gmail.com>
2021-11-25 13:44:45 +0530

    init

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,16 +13,20 @@ class Counter extends Component {
     //     fontWeight: 'bold'
     // };
   render() { 
-      let classes = "badge m-2 badge-";
-      classes += (this.state.count === 0) ? "warning" : "primary";
     return ( 
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="11111111111" /> */}  
-        <span style={{ fontSize: 16 }} className= {classes}>{this.formatCount()}</span>
+        <span style={{ fontSize: 16 }} className= {this.getBadgeClasses}>{this.formatCount()}</span>
         <button className= "btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
 
   formatCount (){
       //return this.state.count==0 ? 'zero' : this.state.count; OR 
