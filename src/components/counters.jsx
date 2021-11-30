98cf58b37fa0d93ddcf4d55a94242d67a23dadc5
Nancy Thomas <linust95@gmail.com>
2021-11-30 17:05:02 +0530

    Destructuring Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,20 +3,21 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+      const { onReset, counters, onDecrement, onIncrement } = this.props;
     return (
       <div>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
 
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDecrement={this.props.onDecrement}
-            onIncrement={this.props.onIncrement}
+            onDecrement={onDecrement}
+            onIncrement={onIncrement}
             // value={counter.value}
             // id={counter.id}
             counter={counter}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,40 @@
-import React, { Component } from "react";
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar
-              <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-            </a>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+import React from "react";
+//////////////Stateless Functional Component/////////////////
+const NavBar = ({totalCounters}) => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+            <span className="badge badge-pill badge-secondary">
+              {totalCounters}
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+// OR //
+
+//////////////////Class Component/////////////////
+// class NavBar extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar navbar-light bg-light">
+//           <div className="container-fluid">
+//             <a className="navbar-brand" href="#">
+//               Navbar
+//               <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+//             </a>
+//           </div>
+//         </nav>
+//       </div>
+//     );
+//   }
+// }
+/////////////////////////////////////////////////
 
 export default NavBar;
